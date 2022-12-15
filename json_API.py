92080178448eecfd62bc0e3b4cc078153c03519d
Strange500 <93034371+Strange500@users.noreply.github.com>
2022-12-15 20:44:46 +0100

    Update json_API.py

--- json_API.py
+++ json_API.py
@@ -22,6 +22,23 @@ def restart():
     return json.dumps({"status" : "ok"}, indent=5)
 
 
+def is_user(req:str):
+    req=urllib.parse.unquote(req)
+    user= req.split("/")[-1].split("u=")[-1]
+    if user in user_list:
+        return json.dumps({"is_user" : True})
+    else:
+        return json.dumps({"is_user" : False})
+
+def cpu_temp():
+    w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+    temperature_infos = w.Sensor()
+    for sensor in temperature_infos:
+        if sensor.SensorType==u'Temperature':
+            return json.dumps({"value" : sensor.value})
+
+
+
 # def temp():
 #     w_temp=wmi.WMI(namespace="root\\wmi")
 #     print(w_temp.)
