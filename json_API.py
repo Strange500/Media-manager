ea2502eff732970d662889f7bd69731c8f1feeda
Strange500 <93034371+Strange500@users.noreply.github.com>
2022-12-16 11:25:07 +0100

    API fixes

--- json_API.py
+++ json_API.py
@@ -34,7 +34,8 @@ def cpu_temp():
     w = wmi.WMI(namespace="root\OpenHardwareMonitor")
     temperature_infos = w.Sensor()
     for sensor in temperature_infos:
-        if sensor.SensorType==u'Temperature':
+        
+        if sensor.SensorType==u'Temperature' and sensor.name == "CPU Package":
             return json.dumps({"value" : sensor.value})
 
 
