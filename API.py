c6ef3cf72ae3c302807f51a94b5d5c86b742b6e5
Strange500 <93034371+Strange500@users.noreply.github.com>
2022-12-17 11:16:05 +0100

    fixes

--- API.py
+++ API.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*
 
-from http.server import BaseHTTPRequestHandler, CGIHTTPRequestHandler, HTTPServer
-from os import curdir, sep, remove,popen
+from http.server import CGIHTTPRequestHandler, HTTPServer
+from os import remove
 
 from json_API import *
-
 
 HTML_FILE_NAME = 'alive'
 PORT_NUMBER = 8080
 
+
 # This class will handles any incoming request from the browser
 class myHandler(CGIHTTPRequestHandler):
-    def comfirm_request(self)->None:
+    def comfirm_request(self) -> None:
         self.send_response(200)
-        self.send_header('Content-type', 'application/json') 
-        self.send_header("Access-Control-Allow-Origin","*")
+        self.send_header('Content-type', 'application/json')
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
 
-    
     def make_response(self) -> None:
         if not "?" in self.path:
-            req_dic={
-                "/alive" : lambda : self.comfirm_request() or self.wfile.write(bytes(alive(),encoding="utf-8")),
-                "/space" : lambda : self.comfirm_request() or self.wfile.write(bytes(space(),encoding="utf-8")),
-                "/restart" : lambda : self.comfirm_request() or self.wfile.write(bytes(restart(),encoding="utf-8")),
-                "/cpu_temp" : lambda : self.comfirm_request() or self.wfile.write(bytes(cpu_temp(),encoding="utf-8"))
+            req_dic = {
+                "/alive": lambda: self.comfirm_request() or self.wfile.write(bytes(alive(), encoding="utf-8")),
+                "/space": lambda: self.comfirm_request() or self.wfile.write(bytes(space(), encoding="utf-8")),
+                "/restart": lambda: self.comfirm_request() or self.wfile.write(bytes(restart(), encoding="utf-8")),
+                "/cpu_temp": lambda: self.comfirm_request() or self.wfile.write(bytes(cpu_temp(), encoding="utf-8")),
+                "/log": lambda: self.comfirm_request() or self.wfile.write(bytes(serv_log(), encoding="utf-8"))
 
             }
             try:
@@ -32,26 +32,26 @@ class myHandler(CGIHTTPRequestHandler):
             except KeyError:
                 self.send_response(404, 'request means nothing: %s' % self.path)
         else:
-            
+
             cgi_dic = {
-                "/is_user" : lambda : self.comfirm_request() or self.wfile.write(bytes(is_user(self.path),encoding="utf-8"))
+                "/is_user": lambda: self.comfirm_request() or self.wfile.write(
+                    bytes(is_user(self.path), encoding="utf-8"))
             }
             try:
-                print("/"+self.path.split("/")[1])
-                cgi_dic["/"+self.path.split("/")[1]]()
+                print("/" + self.path.split("/")[1])
+                cgi_dic["/" + self.path.split("/")[1]]()
             except KeyError:
                 self.send_response(404, 'request means nothing: %s' % self.path)
-        
+
     # Handler for the GET requests
-    
+
     def do_GET(self):
         self.cgi_directories = ["/"]
         self.make_response()
         # if str(self.path)=="/":
 
-
         #     self.path = HTML_FILE_NAME
-        
+
         # try:
         #     with open(curdir + sep + self.path, 'r',encoding="utf-8") as f:
         #         self.send_response(200)
@@ -62,15 +62,14 @@ class myHandler(CGIHTTPRequestHandler):
         # except IOError:
         #     self.send_error(404, 'File Not Found: %s' % self.path)
 
+
 try:
     # Create a web server and define the handler to manage the incoming request
-    
-    print('Started httpserver on port %i.' % PORT_NUMBER)
-    myHandler.cgi_directories= ["/"]
-    #Wait forever for incoming http requests
 
-    
-    
+    print('Started httpserver on port %i.' % PORT_NUMBER)
+    myHandler.cgi_directories = ["/"]
+    # Wait forever for incoming http requests
+
     HTTPServer(('', PORT_NUMBER), myHandler).serve_forever()
 
 except KeyboardInterrupt:
--- json_API.py
+++ json_API.py
@@ -1,57 +1,58 @@
-import os,json,shutil,wmi,urllib.parse
-import psutil
+import json
+import os
+import shutil
+import urllib.parse
+
+import wmi
+
+user_list = ["admin", "Lester", "La mom", "Apo", "Antoine", "DrazZ"]
 
 
-user_list = ["admin","Lester","La mom","Apo","Antoine","DrazZ"]
+def alive() -> json:
+    return json.dumps({"alive": True}, indent=4)
 
-def alive()->json:
-    
-    return  json.dumps({"alive" : True},indent=4)
 
 def space():
-    list_disk,json_list= [f"{i}:\\" for i in [chr(k) for k in range(ord("A"),ord("Z")+1)]],{}
+    list_disk, json_list = [f"{i}:\\" for i in [chr(k) for k in range(ord("A"), ord("Z") + 1)]], {}
     for disk in list_disk:
         if disk == "G:\\":
             pass
         else:
             try:
                 total, used, free = shutil.disk_usage(disk)
-                json_list[disk]={"total" : total,"used" : used,"free" : free}
+                json_list[disk] = {"total": total, "used": used, "free": free}
             except:
                 pass
 
-    return json.dumps(json_list,indent=5)
+    return json.dumps(json_list, indent=5)
+
 
 def restart():
     os.system("shutdown /r")
-    return json.dumps({"status" : "ok"}, indent=5)
+    return json.dumps({"status": "ok"}, indent=5)
 
 
-def is_user(req:str):
-    req=urllib.parse.unquote(req)
-    user= req.split("/")[-1].split("u=")[-1]
+def is_user(req: str):
+    req = urllib.parse.unquote(req)
+    user = req.split("/")[-1].split("u=")[-1]
     if user in user_list:
-        return json.dumps({"is_user" : True})
+        return json.dumps({"is_user": True})
     else:
-        return json.dumps({"is_user" : False})
+        return json.dumps({"is_user": False})
+
 
 def cpu_temp():
     w = wmi.WMI(namespace="root\OpenHardwareMonitor")
     temperature_infos = w.Sensor()
     for sensor in temperature_infos:
-        
-        if sensor.SensorType==u'Temperature' and sensor.name == "CPU Package":
-            return json.dumps({"value" : sensor.value})
+
+        if sensor.SensorType == u'Temperature' and sensor.name == "CPU Package":
+            return json.dumps({"value": sensor.value})
+
 
 def serv_log():
-    with open("log.txt","r") as f:
-        dic={}
+    text = ""
+    with open("log.txt", "r") as f:
         for line in f:
             text = text + line
-    return json.dumps({"value" : text},indent=5)
-
-
-
-
-
-
+    return json.dumps({"value": text}, indent=5)
