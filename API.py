3efde27abb3e77cc20d3c7b21524dbce4e7ea403
Strange500 <93034371+Strange500@users.noreply.github.com>
2023-06-05 22:19:48 +0200

    ADDED API

---
+++ API.py
@@ -0,0 +1,164 @@
+from Database import *
+from flask import Flask, jsonify, request, abort
+from werkzeug.utils import secure_filename
+from flask_cors import CORS
+import psutil, pythoncom, wmi
+class web_API(Server):
+
+    def __init__(self, db: DataBase):
+        super().__init__()
+        self.db = db
+        self.cpu_avg = 0
+        self.cpu_temp_list = []
+
+        self.app = Flask(__name__)
+        self.app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 * 1024  # 10GB limit
+        CORS(self.app)
+
+        @self.app.route("/anime/list")
+        def get_anime():
+            return jsonify(self.db.animes)
+
+        @self.app.route("/anime/dirs")
+        def get_anime_dirs():
+            return jsonify(self.db.anime_dirs)
+
+        @self.app.route("/show/list")
+        def get_show():
+            return jsonify(self.db.shows)
+
+        @self.app.route("/show/dirs")
+        def get_show_dirs():
+            return jsonify(self.db.shows_dirs)
+
+        @self.app.route("/movie/list")
+        def get_movie():
+            return jsonify(self.db.movies)
+
+        @self.app.route("/movie/dirs")
+        def get_movie_dirs():
+            return jsonify(self.db.movie_dirs)
+
+        @self.app.route('/torrent/upload', methods=['POST'])
+        def upload_torrent():
+            self.app.config['UPLOAD_FOLDER'] = Server.conf["torrent_dir"]
+            return upload_file(self.app)
+
+        @self.app.route('/alive')
+        def alive():
+            return jsonify(True)
+
+        @self.app.route("/movie/size")
+        def movie_size():
+            return jsonify(get_total_free_and_used(self.db.movie_dirs))
+
+        @self.app.route("/show/size")
+        def show_size():
+            return jsonify(get_total_free_and_used(self.db.shows_dirs))
+
+        @self.app.route("/anime/size")
+        def anime_size():
+            return jsonify(get_total_free_and_used(self.db.anime_dirs))
+
+        @self.app.route("/movie/nb")
+        def movie_nb():
+            return jsonify({"value": len(self.db.movies)})
+
+        @self.app.route("/show/nb")
+        def show_nb():
+            return jsonify({"value": len(self.db.shows)})
+
+        @self.app.route("/anime/nb")
+        def anime_nb():
+            return jsonify({"value": len(self.db.animes)})
+
+        @self.app.route("/cpu_temp/current")
+        def cpu_temp():
+            pythoncom.CoInitialize()
+            if platform.system() == "Linux":
+                return jsonify({"value": psutil.sensors_temperatures()["k10temp"][0].current})
+            else:
+                w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+                temperature_infos = w.Sensor()
+                for sensor in temperature_infos:
+
+                    if sensor.SensorType == u'Temperature' and sensor.name == "CPU Package":
+                        return jsonify({"value": sensor.value})
+
+        @self.app.route("/cpu_temp/avg")
+        def cpu_avg():
+            self.update_cpu_avg()
+            return jsonify({"value": self.cpu_avg})
+
+        @self.app.route("/tmdb/search", methods=['POST'])
+        def seach_tmdb_show():
+            if request.method == 'POST':
+                if request.form.get("choice") in ["anime", "show"]:
+                    self.search.tv(query=request.form.get("search"))
+                    return jsonify({"results": self.search.results})
+                elif request.form.get("choice") == "movie":
+                    self.search.movie(query=request.form.get("search"))
+                    return jsonify({"results": self.search.results})
+
+        @self.app.route("/request/show", methods=['POST'])
+        def add_show():
+            if request.method == "POST":
+                if request.form.get("id") == "0":
+                    abort(400)
+                if type(request.form.get("id")) == str and request.form.get("id").isnumeric():
+                    if request.form.get("choice") in ["anime", "show"]:
+                        open(os.path.join(VAR_DIR, QUERY_SHOW), "a").write(request.form.get("id") + "\n")
+                        return "ok"
+                    elif request.form.get("choice") == "movie":
+                        open(os.path.join(VAR_DIR, QUERY_MOVIE), "a").write(request.form.get("id") + "\n")
+                        return "ok"
+                    else:
+                        abort(400)
+                else:
+                    abort(400)
+            else:
+                abort(400)
+
+        @self.app.route('/upload', methods=['POST', "OPTIONS"])
+        def upload_file():
+            if 'file' not in request.files:
+                return "Aucun fichier n'a été sélectionné", 400
+
+            file = request.files['file']
+            if file.filename == '':
+                return "Le nom de fichier est vide", 400
+            ch = request.form.get("up_choice")
+            if ch == "anime":
+                dir_save = self.db.to_sort_anime
+            elif ch == "show":
+                dir_save = self.db.to_sort_show
+            elif ch == "movie":
+                dir_save = self.db.to_sort_movie
+            file.save(os.path.join(dir_save, secure_filename(file.filename)))
+
+            return "Téléchargement réussi"
+
+        def upload_file(app: Flask):
+            file = request.files['file']
+            if file:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return 'File uploaded successfully'
+            return 'No file uploaded'
+
+    def run(self):
+        self.app.run(host=IP)
+
+    def update_cpu_temp(self):
+        if platform.system() == "Linux":
+            self.cpu_temp_list.append(psutil.sensors_temperatures()["k10temp"][0].current)
+        else:
+            w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+            temperature_infos = w.Sensor()
+            for sensor in temperature_infos:
+
+                if sensor.SensorType == u'Temperature' and sensor.name == "CPU Package":
+                    self.cpu_temp_list.append(sensor.value)
+
+    def update_cpu_avg(self):
+        self.cpu_avg = round(sum(self.cpu_temp_list) / len(self.cpu_temp_list), 2)
