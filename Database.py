e2f9ac307c0039cb4dffa33a47210ff755a7868d
Strange500 <93034371+Strange500@users.noreply.github.com>
2023-10-14 20:54:17 +0200

    Update Database.py

--- Database.py
+++ Database.py
@@ -558,7 +558,7 @@ class Show(Server):
             Exception: If the update of the TMDB database fails.
         """
         try:
-            self.store_tmdb_info(self.id, show=self.is_show, anime=not self.is_show)
+            self.store_tmdb_info(self.id, show=self.is_show, anime=(not self.is_show))
         except Exception:
             raise Exception("Failed to update TMDB database for the show.")
 
